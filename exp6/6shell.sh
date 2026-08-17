41ede34ec3e5ac57cb823fbfef5549adc7334de9
manonmani-2007 <manonmani1009.cse@gmail.com>
2026-08-17 20:09:53 +0530

    Add files via upload

---
+++ exp6/6shell.sh
@@ -0,0 +1,16 @@
+Shell Script semaphore.sh
+#!/bin/bash
+LOCKFILE="/tmp/mylock"
+
+while [ -f "$LOCKFILE" ]
+do
+    sleep 1
+done
+
+touch "$LOCKFILE"
+
+echo "Entering Critical Section"
+sleep 5
+echo "Leaving Critical Section"
+
+rm -f "$LOCKFILE"
